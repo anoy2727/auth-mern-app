52ebdefd2e425869b00347d97770817bd8bf76a6
Raiyan Rafiz Anoy | MERN Stack Developer <raiyananoy@gmail.com>
2026-04-27 22:39:30 +0600

    Update Signup.js

--- frontend/frontend/src/pages/Signup.js
+++ frontend/frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = "http://localhost:8080/auth/signup"
+            const url = "https://auth-mern-app-api-woad.vercel.app//auth/signup"
             const response = await fetch(url,{
                 method: "POST",
                 headers: {
